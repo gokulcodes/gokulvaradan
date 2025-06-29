206a7636209a419227e8c4023aed93ca4bbb33ab
Gokul Varadan <gokulvaradan2202@gmail.com>
2025-06-29 20:43:04 +0530

    basic commit

---
+++ src/components/blogs/BlogsHub.tsx
@@ -0,0 +1,11 @@
+function BlogsHub() {
+  return (
+    <div>
+      <h1>Blogs Hub</h1>
+      <p>Welcome to the Blogs Hub. Here you can find various blog posts.</p>
+      {/* Additional content can be added here */}
+    </div>
+  );
+}
+
+export default BlogsHub;
